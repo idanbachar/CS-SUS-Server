6975e17103f9544c16c4ac9007304fb153ac3c6b
Idan Bachar <idanac2@gmail.com>
2023-09-24 01:20:28 +0300

    added nodemon support

--- index.ts
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = 3000;
-
-app.get("/", (req, res) => {});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
