469ce6c9dd1a8c36090e51d3145ddcb1b5c94ca9
Idan Bachar <idanac2@gmail.com>
2023-09-26 01:59:38 +0300

    start adding support for sign-in with steam account

--- src/server.ts
+++ src/server.ts
@@ -7,12 +7,63 @@ import {
   checkIsSteamProfileValid,
   getSteamIDFromURL,
 } from "./services/validation";
+import passport from "passport";
+import { Strategy } from "passport-steam";
+import { API_KEY, TOKEN } from "./services/general";
+import session from "express-session";
+const PORT = 4000;
+
+// require("crypto").randomBytes(48, function (err: any, buffer: any) {
+//   TOKEN = buffer.toString("hex");
+// });
 
 const app = express();
+app.use(
+  session({
+    secret: TOKEN!,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj: any, done) => {
+  done(null, obj);
+});
+
+passport.use(
+  new Strategy(
+    {
+      returnURL: `http://localhost:${PORT}/auth/steam/return`,
+      realm: `http://localhost:${PORT}/`,
+      apiKey: API_KEY,
+    },
+    (identifier: any, profile: any, done: any) => {
+      return done(null, profile);
+    }
+  )
+);
+
 app.use("/", express.static("public"));
 app.use(cors());
 
-const PORT = 4000;
+app.get("/auth/steam", passport.authenticate("steam"), (req, res) => {
+  // Handle login success
+  res.json(req.user);
+});
+
+app.get(
+  "/auth/steam/return",
+  passport.authenticate("steam", { failureRedirect: "/" }),
+  (req, res) => {
+    res.redirect("/login-succeed");
+  }
+);
 
 app.get("/getUser", (req: Request, res: Response) => {
   (async () => {
@@ -31,6 +82,14 @@ app.get("/getUser", (req: Request, res: Response) => {
       }
     }
   })();
+});
+
+app.get("/login-succeed", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.json(req.user);
+  } else {
+    res.send("Not authenticated.");
+  }
 });
 
 app.listen(PORT, () => {
--- src/services/general.ts
+++ src/services/general.ts
@@ -1,2 +1,3 @@
 export const API_KEY = process.env.API_KEY;
 export const STEAM_BASE_URL = process.env.STEAM_BASE_URL;
+export const TOKEN = process.env.TOKEN;
